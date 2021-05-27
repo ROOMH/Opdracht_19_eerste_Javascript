738512ce434d2627c32bb9319812001e1636fc06
Romy <80348985+ROOMH@users.noreply.github.com>
2021-05-27 10:00:53 +0200

    Add files via upload

---
+++ 19_Opdracht_eerste_javascript/index.js
@@ -0,0 +1,36 @@
+console.log ("Hello Winc Academy");
+
+let leeftijd = 25;
+console.log (leeftijd);
+
+
+// if is een reserved variable. Dit is niet mogelijk
+
+
+let y = 4;
+let x = y + 2
+console.log (x);
+
+let stringSom = 8;
+console.log ("4+4");
+// Strings optellen kan niet
+
+let addition = 8 + 6;
+console.log (addition);
+
+let subtraction = (6 - 4);
+console.log (subtraction);
+
+let multiplication = (3 * 4);
+console.log (multiplication);
+
+let division = (5 / 2);
+console.log (division);
+
+let modulus = (0,6 - 100);
+console.log (modulus);
+
+
+console.log (typeof 25);
+
+console.log (typeof vijfentwintig);
